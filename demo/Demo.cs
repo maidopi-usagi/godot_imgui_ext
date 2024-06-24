e935e145d99b886f0171eaaf347c95df21046d5a
maidopi-usagi <maidopi@qq.com>
2024-06-25 01:40:36 +0800

    set demo entry as main dockspace and add main menubar

--- demo/Demo.cs
+++ demo/Demo.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 using GodotImGuiExtension;
+using ImGuiGodot;
 using ImGuiNET;
 
 [GlobalClass]
@@ -10,14 +11,42 @@ public partial class Demo : Node
 	private GodotObject _inspectingObject = null;
 	private List<NodePath> _nodePaths = [];
 	
+	[Export(PropertyHint.Range, "0.25, 4.0, 0.1")]
+	public float DpiScale
+	{
+		get => ImGuiGD.Scale;
+		set
+		{
+			Callable.From(() => ImGuiGD.Scale = value).CallDeferred();
+		}
+	}
+
 	public override void _Ready()
 	{
 		ImGui.GetIO().ConfigFlags |= ImGuiConfigFlags.DockingEnable;
 		CallDeferred(Node.MethodName.AddSibling, new InputImeHelper());
+		ImGuiGD.Scale = 2.0f;
 	}
 
 	public override void _Process(double delta)
 	{
+		if (ImGui.BeginMainMenuBar())
+		{
+			if (ImGui.BeginMenu("Panels"))
+			{
+				ImGui.EndMenu();
+			}
+			
+			if (ImGui.BeginMenu("About"))
+			{
+				ImGui.EndMenu();
+			}
+			
+			ImGui.EndMainMenuBar();
+		}
+		
+		ImGui.DockSpaceOverViewport();
+		
 		ImGui.ShowDemoWindow();
 
 		if (SceneTreeViewer.DrawSceneTree(ref _nodePaths))
